3ec9aa1420a6e271f385f664e5d3a25fb505fdc8
Zaxar Skadi <alejnikov01@gmail.com>
2025-01-21 16:03:31 +0300

    accel polling in loop

--- imu_6500/imu_6500.ino
+++ imu_6500/imu_6500.ino
@@ -4,6 +4,13 @@ const int MPU_addr = 0x68; // адрес датчика
 // [accX, accY, accZ, temp, gyrX, gyrY, gyrZ]
 // acc - ускорение, gyr - угловая скорость, temp - температура (raw)
 int16_t data[7];  
+int16_t acc_X_data;
+int16_t acc_Y_data;
+int16_t acc_Z_data;
+int16_t gyro_X_data;
+int16_t gyro_Y_data;
+int16_t gyro_Z_data;
+
 void setup() {
   // инициализация
   Wire.begin();
@@ -15,21 +22,59 @@ void setup() {
   Serial.begin(9600);
 }
 void loop() {
-  getData();  // получаем
-  // выводим 
-  for (byte i = 0; i < 7; i++) {
-    Serial.print(data[i]);
-    Serial.print('\t');
-  }
-  Serial.println();
-  delay(200);
-}
-void getData() {
   Wire.beginTransmission(MPU_addr);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
-  for (byte i = 0; i < 7; i++) {
-    data[i] = Wire.read() << 8 | Wire.read();
+  Wire.write(0x3b);
+  Wire.endTransmission();
+
+  Wire.requestFrom(MPU_addr, 2);
+
+  while(Wire.available()){
+    int16_t acc_x = Wire.read() << 8 | Wire.read();
+    Serial.println(acc_x, DEC);
   }
+
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3D);
+  Wire.endTransmission();
+
+  Wire.requestFrom(MPU_addr, 2);
+
+  while(Wire.available()){
+    int16_t acc_y = Wire.read() << 8 | Wire.read();
+    Serial.println(acc_y, DEC);
+  }
+
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3F);
+  Wire.endTransmission();
+
+  Wire.requestFrom(MPU_addr, 2);
+
+  while(Wire.available()){
+    int16_t acc_z = Wire.read() << 8 | Wire.read();
+    Serial.println(acc_z, DEC);
+  }
+
+  // Wire.requestFrom(MPU_addr, 2);
+
+  // while(Wire.available()){
+  //   int16_t acc_y = Wire.read() << 8 | Wire.read();
+  //   Serial.println(acc_y, DEC);
+  // }
+
+  delay(500);
+  // void get_ACC_X();  // получаем
+  // // выводим 
+  // Serial.print(acc_X_data);
+  // Serial.println();
+  // delay(200);
+  // Wire.endTransmission();
 }
+
+// void get_ACC_X(){
+//   Wire.beginTransmission(MPU_addr);
+//   Wire.write(0x3C);
+//   Wire.endTransmission(false);
+//   Wire.requestFrom(MPU_addr, 1, true);
+//   acc_X_data = Wire.read();
+// }
