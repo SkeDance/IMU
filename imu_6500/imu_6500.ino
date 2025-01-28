529cdedad279bdd8d84c2fc9c31adafe3f07e6e5
Zaxar Skadi <qoupskd@gmail.com>
2025-01-28 16:30:25 +0300

    Double integration of accelerations

--- imu_6500/imu_6500.ino
+++ imu_6500/imu_6500.ino
@@ -1,130 +1,210 @@
 #include "Wire.h"
+#include "math.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 #define F_CPU 16000000
+#define TEST_LED  7
 
 const int MPU_addr = 0x68; // адрес датчика
 // массив данных
 // [accX, accY, accZ, temp, gyrX, gyrY, gyrZ]
 // acc - ускорение, gyr - угловая скорость, temp - температура (raw)
-int16_t data[7];  
-int16_t acc_X_data;
-int16_t acc_Y_data;
-int16_t acc_Z_data;
-int16_t gyro_X_data;
-int16_t gyro_Y_data;
-int16_t gyro_Z_data;
+//int16_t data[7];  
 
 volatile int flag = 0;
 volatile float OCR1 = 0;
+float g = 9.81;
 
-void getAccelX(){
+float current_aX;
+float previous_aX;
+float current_aY;
+float previous_aY;
+float current_aZ;
+float previous_aZ;
+
+float velocityX;
+float prev_velocityX;
+float velocityY;
+float prev_velocityY;
+float velocityZ;
+float prev_velocityZ;
+
+float X;
+float Y;
+float Z;
+
+float wX;
+float wY;
+float wZ;
+
+float PITCH;
+float ROLL;
+float YAW;
+
+float getAccelX(){
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3b);
   Wire.endTransmission();
 
   Wire.requestFrom(MPU_addr, 2);
 
+  float acc_X_real;
+
   while(Wire.available()){
     int16_t acc_x = Wire.read() << 8 | Wire.read();
-    float acc_X_real = (float)acc_x / 32768.0 * 2.0;
-    Serial.print("aX = "); Serial.println(acc_X_real, 6);
+    acc_X_real = (float)acc_x / 32768.0 * 2.0;
   }
+  return acc_X_real;
 }
 
-void getAccelY(){
+float getAccelY(){
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3D);
   Wire.endTransmission();
 
   Wire.requestFrom(MPU_addr, 2);
 
+  float acc_Y_real;
+
   while(Wire.available()){
     int16_t acc_y = Wire.read() << 8 | Wire.read();
-    float acc_Y_real = (float)acc_y / 32768.0 * 2.0;
-    Serial.print("aY = "); Serial.println(acc_Y_real, 6);
+    acc_Y_real = (float)acc_y / 32768.0 * 2.0;
   }
+  return acc_Y_real;
 }
 
-void getAccelZ(){
+float getAccelZ(){
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3F);
   Wire.endTransmission();
 
   Wire.requestFrom(MPU_addr, 2);
 
+  float acc_Z_real;
+
   while(Wire.available()){
     int16_t acc_z = Wire.read() << 8 | Wire.read();
-    float acc_Z_real = (float)acc_z / 32768.0 * 2.0;
-    Serial.print("aZ = "); Serial.println(acc_Z_real, 6);
+    acc_Z_real = (float)acc_z / 32768.0 * 2.0;
   }
+  return acc_Z_real;
 }
 
-void getGyroX(){
+float getGyroX(){
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x43);
   Wire.endTransmission();
 
   Wire.requestFrom(MPU_addr, 2);
 
+  float gyro_X_real;
+
   while(Wire.available()){
     int16_t gyro_x = Wire.read() << 8 | Wire.read();
-    float gyro_X_real = (float)gyro_x / 32768.0 * 250.0;
-    Serial.print("gX = "); Serial.println(gyro_X_real, 6);
+    gyro_X_real = (float)gyro_x / 32768.0 * 250.0;
   }
+  return gyro_X_real;
 }
 
-void getGyroY(){
+float getGyroY(){
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x45);
   Wire.endTransmission();
 
   Wire.requestFrom(MPU_addr, 2);
 
+  float gyro_Y_real;
+
   while(Wire.available()){
     int16_t gyro_y = Wire.read() << 8 | Wire.read();
-    float gyro_Y_real = (float)gyro_y / 32768.0 * 250.0;
-    Serial.print("gY = "); Serial.println(gyro_Y_real, 6);
+    gyro_Y_real = (float)gyro_y / 32768.0 * 250.0;
   }
+  return gyro_Y_real;
 }
 
-void getGyroZ(){
+float getGyroZ(){
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x47);
   Wire.endTransmission();
 
   Wire.requestFrom(MPU_addr, 2);
 
+  float gyro_Z_real;
+
   while(Wire.available()){
     int16_t gyro_z = Wire.read() << 8 | Wire.read();
-    float gyro_Z_real = (float)gyro_z / 32768.0 * 250.0;
-    Serial.print("gZ = "); Serial.println(gyro_Z_real, 6); 
+    gyro_Z_real = (float)gyro_z / 32768.0 * 250.0; 
   }
+  return gyro_Z_real;
+}
+
+float getX(){
+  previous_aX = current_aX;
+  current_aX = getAccelX();
+  prev_velocityX = velocityX;
+  velocityX = velocityX + (current_aX + previous_aX) * 0.01;
+  X = X + (velocityX + prev_velocityX) * 0.01;
+  return X;
+}
+
+float getY(){
+  previous_aY = current_aY;
+  current_aY = getAccelY();
+  prev_velocityY = velocityY;
+  velocityY = velocityY + (current_aY + previous_aY) * 0.01;
+  Y = Y + (velocityY + prev_velocityY) * 0.01;
+  return Y;
+}
+
+float getZ(){
+  previous_aZ = current_aZ;
+  current_aZ = getAccelZ();
+  prev_velocityZ = velocityZ;
+  velocityZ = velocityZ + (current_aZ + previous_aZ) * 0.01;
+  Z = Z + (velocityZ + prev_velocityZ) * 0.01;
+  return Z;
+}
+
+float getPITCH(){
+  PITCH = -getAccelX() / g;
+  return PITCH;
+}
+
+float getROLL(){
+  ROLL = -getAccelY() / g;
+  return ROLL;
+}
+
+float getYAW(){
+  YAW = -atan(getGyroX() / getGyroY());
+  return YAW;
 }
 
 void setup() {
   Serial.begin(9600); 
   
-  InitTimer(0.1);
+  InitTimer(0.01);
   InitMPU();
 
-  pinMode(7, OUTPUT);
+  pinMode(TEST_LED, OUTPUT);
 
+  YAW = getYAW();
 }
+
 void loop() {
   
   if(flag == 1){
-    
-  //poll Accelerometr
-  getAccelX();
-  getAccelY();
-  getAccelZ();
 
+  //poll Accelerometr
+  X = getX();
+  Y = getY();
+  Z = getZ();
+  Serial.print("X = "); Serial.print(X); Serial.print("    Y = "); Serial.print(Y); Serial.print("    Z = "); Serial.println(Z);
+ 
   //poll Gyro 
-  getGyroX();
-  getGyroY();
-  getGyroZ();
+  wX = getGyroX();
+  wY = getGyroY();
+  wZ = getGyroZ(); 
 
   flag = 0;
   }
@@ -143,8 +223,7 @@ void InitTimer(float time){
 ISR(TIMER1_COMPA_vect){
   flag++;
   if(flag == 1){
-    //led for interrupt test
-    digitalWrite(7, digitalRead(7) ^ 1);
+    digitalWrite(TEST_LED, digitalRead(TEST_LED) ^ 1);
   }
 }
 
@@ -159,7 +238,6 @@ void InitMPU(){
 float Timer1ClockSetup(float time){
   float resolution = (1.0 / (F_CPU / 1024.0));
   OCR1 = time / resolution;
-  Serial.println(OCR1);
   return OCR1;
 }
 
