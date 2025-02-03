64a88c6544b8c80b5c75c22905280969967ccc96
Zaxar Skadi <qoupskd@gmail.com>
2025-02-03 15:36:32 +0300

    Completed first iteration

--- imu_6500/imu_6500.ino
+++ imu_6500/imu_6500.ino
@@ -77,6 +77,12 @@ float PITCH_0;
 float ROLL_0;
 float YAW_0;
 
+float PITCH;
+float ROLL;
+float YAW;
+
+float C_0;
+
 float getAccelX(){
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3b);
@@ -215,50 +221,45 @@ void bodyToLocal(float aX, float aY, float aZ){
 }
 
 float matrix(int i, int j){
-  matrix_LL[0][0] = 1;//cos(ROLL_0) * cos(YAW_0) + sin(PITCH_0) * sin(ROLL_0) * sin(YAW_0);
-  matrix_LL[0][1] = 2;//-cos(ROLL_0) * sin(YAW_0) + sin(PITCH_0) * sin(ROLL_0) * cos(YAW_0);
-  matrix_LL[0][2] = 3;//cos(PITCH_0) * sin(ROLL_0);
-  matrix_LL[1][0] = 4;//cos(PITCH_0) * sin(YAW_0);
-  matrix_LL[1][1] = 5;//cos(PITCH_0) * cos(YAW_0);
-  matrix_LL[1][2] = 6;//sin(PITCH_0);
-  matrix_LL[2][0] = 7;//sin(ROLL_0) * cos(YAW_0) - sin(PITCH_0) * cos(ROLL_0) * sin(YAW_0);
-  matrix_LL[2][1] = 8;//-sin(ROLL_0) * sin(YAW_0) - sin(PITCH_0) * cos(ROLL_0) * sin(YAW_0);
-  matrix_LL[2][2] = 9;//cos(PITCH_0) * cos(ROLL_0);
-  // for(int i = 0; i < 3; i++){
-  //   for(int j = 0; j < 3; j++){
-  //     Serial.println(matrix_LL[i][j], 6);
-  //   }
-  // }
+  matrix_LL[0][0] = cos(ROLL_0) * cos(YAW_0) + sin(PITCH_0) * sin(ROLL_0) * sin(YAW_0);
+  matrix_LL[0][1] = -cos(ROLL_0) * sin(YAW_0) + sin(PITCH_0) * sin(ROLL_0) * cos(YAW_0);
+  matrix_LL[0][2] = cos(PITCH_0) * sin(ROLL_0);
+  matrix_LL[1][0] = cos(PITCH_0) * sin(YAW_0);
+  matrix_LL[1][1] = cos(PITCH_0) * cos(YAW_0);
+  matrix_LL[1][2] = sin(PITCH_0);
+  matrix_LL[2][0] = sin(ROLL_0) * cos(YAW_0) - sin(PITCH_0) * cos(ROLL_0) * sin(YAW_0);
+  matrix_LL[2][1] = -sin(ROLL_0) * sin(YAW_0) - sin(PITCH_0) * cos(ROLL_0) * sin(YAW_0);
+  matrix_LL[2][2] = cos(PITCH_0) * cos(ROLL_0);
   return matrix_LL[i][j];
 }
 
 float matrix_W_ENUp(int i, int j){
-  matrix_W_LL[0][0] = 10;//0;
-  matrix_W_LL[0][1] = 20;//-wUp;
-  matrix_W_LL[0][2] = 30;//wN;
-  matrix_W_LL[1][0] = 40;//wUp;
-  matrix_W_LL[1][1] = 50;//0;
-  matrix_W_LL[1][2] = 60;//-wE;
-  matrix_W_LL[2][0] = 70;//-wN;
-  matrix_W_LL[2][1] = 80;//wE;
-  matrix_W_LL[2][2] = 90;//0;
+  matrix_W_LL[0][0] = 0;
+  matrix_W_LL[0][1] = -wUp;
+  matrix_W_LL[0][2] = wN;
+  matrix_W_LL[1][0] = wUp;
+  matrix_W_LL[1][1] = 0;
+  matrix_W_LL[1][2] = -wE;
+  matrix_W_LL[2][0] = -wN;
+  matrix_W_LL[2][1] = wE;
+  matrix_W_LL[2][2] = 0;
   return matrix_W_LL[i][j];
 }
 
 float matrix_W_DUS(int i, int j){
-  matrix_W_B[0][0] = 11;//0;
-  matrix_W_B[0][1] = 21;//-wZ;
-  matrix_W_B[0][2] = 31;//wY;
-  matrix_W_B[1][0] = 41;//wZ;
-  matrix_W_B[1][1] = 51;//0;
-  matrix_W_B[1][2] = 61;//-wX;
-  matrix_W_B[2][0] = 71;//-wY;
-  matrix_W_B[2][1] = 81;//wX;
-  matrix_W_B[2][2] = 91;//0;
+  matrix_W_B[0][0] = 0;
+  matrix_W_B[0][1] = -wZ;
+  matrix_W_B[0][2] = wY;
+  matrix_W_B[1][0] = wZ;
+  matrix_W_B[1][1] = 0;
+  matrix_W_B[1][2] = -wX;
+  matrix_W_B[2][0] = -wY;
+  matrix_W_B[2][1] = wX;
+  matrix_W_B[2][2] = 0;
   return matrix_W_B[i][j];
 }
 
-void Puasson(){
+void Poisson(){
   for(int i = 0, j = 0, k = 0; i <= 2; i++){
     for(; j <= 2; j++){
       for(; k <= 2; k++){
@@ -313,20 +314,29 @@ void loop() {
     wN = velocityX / R_longitude;
     wUp = velocityX / R_longitude * tan(latitude_0);
 
+    Poisson();
+
+    C_0 = sqrt(((NEW_LL_MATRIX[2][0] * NEW_LL_MATRIX[2][0]) + (NEW_LL_MATRIX[2][2] * NEW_LL_MATRIX[2][2])));
+
+    PITCH = fmod((NEW_LL_MATRIX[2][1] / C_0), 90.0);
+    ROLL = fmod((NEW_LL_MATRIX[2][0] / NEW_LL_MATRIX[2][2]), 180.0);
+    YAW = fmod((NEW_LL_MATRIX[0][1] / NEW_LL_MATRIX[1][1]), 360.0);
+
     Serial.print("PITCH_0 = "); Serial.print(PITCH_0, 6); Serial.print("    ROLL_0 = "); Serial.print(ROLL_0, 6); Serial.print("    YAW_0 = "); Serial.println(YAW_0, 6);
 
     Serial.print("aX = "); Serial.print(Acc_matrix_ENUp[0][0]); Serial.print("    aY = "); Serial.print(Acc_matrix_ENUp[1][0]); Serial.print("    aZ = "); Serial.println(Acc_matrix_ENUp[2][0]);
 
     Serial.print("X1 = "); Serial.print(X); Serial.print("    Y1 = "); Serial.print(Y); Serial.print("    Z1 = "); Serial.println(Z);
 
-
-    Puasson();
-
     for(int i = 0; i < 3; i++){
       for(int j = 0; j < 3; j++){
         Serial.println(NEW_LL_MATRIX[i][j], 6);
       }
     }
+
+    Serial.print("C_0 = "); Serial.println(C_0, 6);
+
+    Serial.print("PITCH = "); Serial.print(PITCH, 6); Serial.print("    ROLL = "); Serial.print(ROLL, 6); Serial.print("    YAW = "); Serial.println(YAW, 6);
 
   }
 
